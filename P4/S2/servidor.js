527c89772e08b868bedd8209a47890bb0c3f4231
Miguel Angel <posadasarraez@gmail.com>
2020-05-15 11:50:14 +0200

    Añadiendo sensores y actuadores extra

--- P4/S2/servidor.js
+++ P4/S2/servidor.js
@@ -9,12 +9,22 @@ var MongoServer = require('mongodb').Server;
 
 var LUM_ACTUAL = 50
 var TEMP_ACTUAL = 25
+var HUM_ACTUAL = 50
+var SUCIEDAD_ACTUAL = 0
 
 var UMBRAL_TEMP = 30
+var UMBRAL_TEMP_MIN = 10
 var UMBRAL_LUM = 20 
+var UMBRAL_HUMEDAD = 20
+var UMBRAL_SUCIEDAD = 20
 
 var persiana = false  //false bajada - true subida 
 var aireAcondicionado = false  //false apagado - true encendido
+var calefaccion = false //false apagado - true encendido
+var robotAspirador = false	//false apagado - true encendido
+var cerraduraInteligente = false	//false apagado - true encendido
+var humidificador = false //false apagado - true encendido
+var ventana = false //false cerrrada - true abierta
 var agenteDomotico = true //Variable que apaga o enciende el agente domotico
 
 
@@ -23,28 +33,96 @@ function agente()
 	if(agenteDomotico)
 	{
 		var mensajes = Array();
+		var d = new Date();
+
+		if(d.getHours() >= 22 && d.getMinutes() >= 30  && !cerraduraInteligente)
+		{
+			cerraduraInteligente = true;
+			mensajes.push("El agente ha encendido la cerradura inteligente porque ha llegado la hora programada</br>");
+		}
 
 		if (TEMP_ACTUAL > UMBRAL_TEMP && !aireAcondicionado)
 		{
-			aireAcondicionado = true
-			mensajes.push("El agente ha encendido el aire acondicionado porque la temperatura es muy alta\n");
+			aireAcondicionado = true;
+			mensajes.push("El agente ha encendido el aire acondicionado porque la temperatura es muy alta</br>");
+			if(!humidificador)
+			{
+				humidificador = true;
+				mensajes.push("El agente ha encendido el humidificador porque el aire acondicionado está en uso</br>");
+			}
+			if(calefaccion)
+			{
+				calefaccion = false;
+				mensajes.push("El agente ha apagado la calefacción para evitar tener el aire acondicionado y la calefacción encendidos simultáneamente</br>");
+			}
+				
 		}
-		else if (TEMP_ACTUAL < UMBRAL_TEMP && aireAcondicionado)
+		else if (TEMP_ACTUAL < UMBRAL_TEMP && TEMP_ACTUAL > UMBRAL_TEMP_MIN && aireAcondicionado)
 		{
-			aireAcondicionado = false
-			mensajes.push("El agente ha apagado el aire acondicionado porque la temperatura es inferior al umbral\n");
+			aireAcondicionado = false;
+			mensajes.push("El agente ha apagado el aire acondicionado porque la temperatura es inferior al umbral </br>");
+		}
+
+		if(TEMP_ACTUAL < UMBRAL_TEMP_MIN && !calefaccion)
+		{
+			calefaccion = true;
+			mensajes.push("El agente ha encendido la calefacción porque hace frío </br>")
+			if(aireAcondicionado)
+			{
+				aireAcondicionado = false;
+				mensajes.push("El agente ha apagado el aire acondicionado para evitar tener el aire acondicionado y la calefacción encendidos simultáneamente</br>");
+			}
+				
+
+		}else if (TEMP_ACTUAL > UMBRAL_TEMP_MIN && calefaccion)
+		{
+			calefaccion = false;
+			mensajes.push("El agente ha apgado la calefacción porque ya no es necesaria </br>")
+		}
+
+		if((aireAcondicionado | calefaccion) && ventana)
+		{
+			ventana = false;
+			mensajes.push("El agente ha cerrado la ventana porque hay algún dispositivo de control de temperatura en uso </br>");
+
+		}else if (!aireAcondicionado && !calefaccion && !ventana)
+		{
+			ventana = true;
+			mensajes.push("El agente ha abierto la ventana porque no hay ningún dispositivo de control de temperatura en uso </br>");
 		}
 
 		if (LUM_ACTUAL < UMBRAL_LUM && !persiana)
 		{
-			mensajes.push("El agente ha subido la persiana porque la luminosidad es inferior al umbral\n");
-			persiana = true
+			mensajes.push("El agente ha subido la persiana porque la luminosidad es inferior al umbral </br>");
+			persiana = true;
 		}
 
-		if (TEMP_ACTUAL > UMBRAL_TEMP && LUM_ACTUAL > UMBRAL_LUM)
+		if (TEMP_ACTUAL > UMBRAL_TEMP && LUM_ACTUAL > UMBRAL_LUM && persiana)
 		{
-			persiana = false
-			mensajes.push("El agente ha bajado la persiana porque la temperatura y la luminosidad son muy altas\n");
+			persiana = false;
+			mensajes.push("El agente ha bajado la persiana porque la temperatura y la luminosidad son muy altas </br>");
+		}
+
+		if(SUCIEDAD_ACTUAL > UMBRAL_SUCIEDAD && !robotAspirador)
+		{
+			robotAspirador = true;
+			mensajes.push("Recoge lo que tengas por el suelo, se ha encendido el robot aspirador. ¡ Cuidado con los cables ! </br>");
+
+		}else if (SUCIEDAD_ACTUAL < UMBRAL_SUCIEDAD && robotAspirador)
+		{
+			robotAspirador = false;
+			mensajes.push("El robot aspirador a terminado, ¡ Recuerda vaciar el depósito ! </br>")
+		}
+
+		if(HUM_ACTUAL < UMBRAL_HUMEDAD && !humidificador)
+		{
+			humidificador = true;
+			mensajes.push("El agente ha encendido el humidificador porque los niveles de humedad están por debajo del umbral </br>");
+
+		} else if(HUM_ACTUAL > UMBRAL_HUMEDAD && humidificador && !aireAcondicionado)
+		{
+			humidificador = false;
+			mensajes.push("El agente ha apagado el humidificador porque los niveles de humedad están por encima del umbral y el aire acondicionado está apagado </br>");
 		}
 		
 		return mensajes;
@@ -111,9 +189,16 @@ MongoClient.connect("mongodb://localhost:27017/", function(err, db){
 		u.emit('valoresSensores', {
 			temperatura: TEMP_ACTUAL,
 			luminosidad: LUM_ACTUAL,
+			humedad: HUM_ACTUAL,
+			suciedad: SUCIEDAD_ACTUAL,
 			estadoAireAcondicionado: aireAcondicionado,
 			estadoPersiana: persiana,
 			estadoAgenteDomotico: agenteDomotico,
+			estadoCalefaccion: calefaccion,
+			estadoAspirador: robotAspirador,
+			estadoVentana: ventana,
+			estadoHumidificador: humidificador, 
+			estadoCerradura: cerraduraInteligente,
 			mensajesAgente : 'El agente no ha actuado todavía' 
 		});
 
@@ -134,6 +219,48 @@ MongoClient.connect("mongodb://localhost:27017/", function(err, db){
 			obtenerRegistro() //Actualizamos el registro de la pagina de los sensores
 		})
 
+		u.on('ventana',function(){
+			ventana = !ventana;
+			if(ventana) mensaje = 'El usuario ha abierto la ventana manualmente' 
+			else mensaje = 'El usuario ha cerrado la ventana manualmente'
+			actualizarValoresUsuarios(mensaje);
+			obtenerRegistro() //Actualizamos el registro de la pagina de los sensores
+		})
+
+		u.on('aspirador',function(){
+			robotAspirador = !robotAspirador;
+			if(robotAspirador) mensaje = 'El usuario ha encendido el robot aspirador manualmente' 
+			else mensaje = 'El usuario ha apagado el robot aspirador manualmente'
+			actualizarValoresUsuarios(mensaje);
+			obtenerRegistro() //Actualizamos el registro de la pagina de los sensores
+		})
+
+		u.on('humidificador',function(){
+			humidificador = !humidificador;
+			if(humidificador) mensaje = 'El usuario ha encendido el humidificador manualmente' 
+			else mensaje = 'El usuario ha apagado el humidificador manualmente'
+			actualizarValoresUsuarios(mensaje);
+			obtenerRegistro() //Actualizamos el registro de la pagina de los sensores
+		})
+
+
+		u.on('calefaccion',function(){
+			calefaccion = !calefaccion;
+			if(calefaccion) mensaje = 'El usuario ha encendido la calefacción manualmente' 
+			else mensaje = 'El usuario ha apagado la calefacción manualmente'
+			actualizarValoresUsuarios(mensaje);
+			obtenerRegistro() //Actualizamos el registro de la pagina de los sensores
+		})
+
+		u.on('cerradura',function(){
+			cerraduraInteligente = !cerraduraInteligente;
+			if(cerraduraInteligente) mensaje = 'El usuario ha encendido la cerradura inteligente manualmente' 
+			else mensaje = 'El usuario ha apagado la cerradura inteligente manualmente'
+			actualizarValoresUsuarios(mensaje);
+			obtenerRegistro() //Actualizamos el registro de la pagina de los sensores
+		})
+
+
 		u.on('valores-captados',function(data){
 			if(data.sensor == 'temperatura')
 			{
@@ -142,6 +269,14 @@ MongoClient.connect("mongodb://localhost:27017/", function(err, db){
 			}else if(data.sensor == 'luminosidad')
 			{
 				LUM_ACTUAL = data.valor;
+
+			}else if(data.sensor == 'suciedad')
+			{
+				SUCIEDAD_ACTUAL = data.valor;
+
+			}else if(data.sensor == 'humedad')
+			{
+				HUM_ACTUAL = data.valor;
 			}
 			var mensajes = agente();
 			actualizarValoresUsuarios(mensajes);
@@ -197,9 +332,16 @@ MongoClient.connect("mongodb://localhost:27017/", function(err, db){
 			io.sockets.emit('valoresSensores', {
 				temperatura: TEMP_ACTUAL,
 				luminosidad: LUM_ACTUAL,
+				humedad: HUM_ACTUAL,
+				suciedad: SUCIEDAD_ACTUAL,
 				estadoAireAcondicionado: aireAcondicionado,
 				estadoPersiana: persiana,
 				estadoAgenteDomotico: agenteDomotico,
+				estadoCalefaccion: calefaccion,
+				estadoAspirador: robotAspirador,
+				estadoVentana: ventana,
+				estadoHumidificador: humidificador,  
+				estadoCerradura: cerraduraInteligente,
 				mensajesAgente : mensajes
 			});
 		}
